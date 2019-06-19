e7568634fc09fcefbd4c02174cbfe69201c09197
Bill Schreiber <bills@printnetusa.com>
2019-06-19 00:14:49 -0500

    final code

--- PyBank/main.py
+++ PyBank/main.py
@@ -7,9 +7,12 @@ budget = os.path.join("Resources", "budget_data.csv")
 months = 0
 PL = 0
 DailyPL = 0
+PriorPL = 0
 Openval = 0
 Closeval = 0
 AvgChange = 0
+AvgChangeSum = 0
+AvgChangeCtr = 0
 GreatIncDt = ""
 GreatIncVal = 0
 GreatDecDt = ""
@@ -22,6 +25,7 @@ with open(budget, newline='') as csvfile:
     for row in csvreader:
         months += 1
         PL += int(row[1])
+        PriorPL = DailyPL
         DailyPL = (int(row[1]) - Closeval)
         if DailyPL > GreatIncVal:
             GreatIncDt = row[0]
@@ -32,15 +36,20 @@ with open(budget, newline='') as csvfile:
         if months == 1:
             Openval = int(row[1])
         Closeval = int(row[1])
-    AvgChange = (PL / months)
+        if months >= 2:
+            AvgChangeSum += (DailyPL)
+            AvgChangeCtr += 1
+    AvgChange = round((AvgChangeSum / AvgChangeCtr),2)
 
+print(AvgChangeSum)
+print(AvgChangeCtr)
 print()
 print()
 print("Financial Analysis")
 print("----------------------------")
 print("Total Months: " + str(months))
 print("Total: $" + str(PL))
-print("Average Change: $" + str(AvgChange/months))
+print("Average Change: $" + str(AvgChange))
 print(f"Greatest Increase in Profits: " + GreatIncDt + " ($" + str(GreatIncVal) + ")")
 print(f"Greatest Decrease in Profits: " + GreatDecDt + " ($" + str(GreatDecVal) + ")") 
 print()
@@ -51,7 +60,7 @@ print("Financial Analysis", file=open("PyBankWBS.txt", "w"))
 print("----------------------------", file=open("PyBankWBS.txt", "a"))
 print("Total Months: " + str(months), file=open("PyBankWBS.txt", "a"))
 print("Total: $" + str(PL), file=open("PyBankWBS.txt", "a"))
-print("Average Change: $" + str(AvgChange/months), file=open("PyBankWBS.txt", "a"))
+print("Average Change: $" + str(AvgChange), file=open("PyBankWBS.txt", "a"))
 print(f"Greatest Increase in Profits: " + GreatIncDt + " ($" + str(GreatIncVal) + ")", file=open("PyBankWBS.txt", "a"))
 print(f"Greatest Decrease in Profits: " + GreatDecDt + " ($" + str(GreatDecVal) + ")", file=open("PyBankWBS.txt", "a")) 
 
